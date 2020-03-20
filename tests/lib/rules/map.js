195e07f1da5309d605db48dee70afac9a484d562
Artem Loiko <artuom130@gmail.com>
2020-03-20 09:41:54 +0300

    Update tests

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -15,65 +15,101 @@ var rule = require("../../../lib/rules/map"),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
 ruleTester.run("map", rule, {
   valid: [
-    `['1', '2', '3'].map(console.log)`,
-    `[[1, 2, 3], [1, 2, 3]].map(console.log)`,
-    `[1, 2, 3].map(console.log)`,
+    `[1, 2, 3].map((item) => item)`,
     `
-      var obj = {
-        a: 1,
-        b: 2,
-        c: 3
-      };
-      var fn = function(item) { return item + 1; };
-      var mappedObj = _.map(obj, fn);
-      var obj = [1, 2, 3];
+    function someMapperWithCondition(collection) {
+      const fn = item => item + 1;
+    
+      return Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn);
+    }
     `,
-    `_.map("string", console.log)`,
     `
-      var fn = function(item) { return item + 1; };
-      _.map({}, fn)
+    function someMapperWithLongCondition(collection) {
+      const fn = item => item + 1;
+      
+      if (Array.isArray(collection)) {
+        return collection.map(fn);
+      } else {
+        return _.map(collection, fn);
+      }
+    }
+    `,
+    `
+    function someMapperWithReverse(collection) {
+      const fn = item => item + 1;
+    
+      return !Array.isArray(collection) ? _.map(collection, fn) : collection.map(fn);
+    }
+    `,
+    `
+    const obj = { a: 1, b: 2, c: 3 };
+    const mappedObj = _.map(obj, fn);
     `
   ],
 
   invalid: [
     {
       code: `
-        var mappedArr = _.map([1, 2, 3], fn);
+      let letArr = [1, 2, 3];
+      var varArr = [1, 2, 3];
+      const fn = item => item + 1;
+      const letArrMapped = _.map(letArr, (item) => item);
+      const varArrMapped = _.map(varArr, fn);
       `,
-      errors: [
-        {
-          message: "Replace with native Array#map"
-        }
-      ]
+      errors: [{ message: "Add native Array#map" }, { message: "Add native Array#map" }],
+      output: `
+      let letArr = [1, 2, 3];
+      var varArr = [1, 2, 3];
+      const fn = item => item + 1;
+      const letArrMapped = Array.isArray(letArr) ? letArr.map((item) => item) : _.map(letArr, (item) => item);
+      const varArrMapped = Array.isArray(varArr) ? varArr.map(fn) : _.map(varArr, fn);
+      `
     },
     {
       code: `
-        var arr = [1, 2, 3];
-        var fn = function(item) { return item + 1; };
-        var mappedArr = _.map(arr, fn);
+      const constArr = [1, 2, 3];
+      const fn = item => item + 1;
+      const constArrMapped = _.map(constArr, fn);
+      const literalMapped = _.map([1, 2, 3], fn);
       `,
-      errors: [
-        {
-          message: "Replace with native Array#map"
-        }
-      ]
+      errors: [{ message: "Add native Array#map" }, { message: "Add native Array#map" }],
+      output: `
+      const constArr = [1, 2, 3];
+      const fn = item => item + 1;
+      const constArrMapped = constArr.map(fn);
+      const literalMapped = [1, 2, 3].map(fn);
+      `
     },
     {
       code: `
-        var arr;
-        var arr = [1, 2, 3];
-        var fn = function(item) { return item + 1; };
-        var mappedArr = _.map(arr, fn);
-        var arr = "string";
+      const constArr = [1, 2, 3];
+      const fn = item => item + 1;
+      const mapperWithOuterVar = () => {
+        return _.map(constArr, fn);
+      };
       `,
-      errors: [
-        {
-          message: "Replace with native Array#map"
-        }
-      ]
+      errors: [{ message: "Add native Array#map" }],
+      output: `
+      const constArr = [1, 2, 3];
+      const fn = item => item + 1;
+      const mapperWithOuterVar = () => {
+        return constArr.map(fn);
+      };
+      `
+    },
+    {
+      code: `
+      var varobj = { a: 1, b: 2, c: 3 };
+      const mappedVarObj = _.map(varobj, fn);
+      `,
+      errors: [{ message: "Add native Array#map" }],
+      output: `
+      var varobj = { a: 1, b: 2, c: 3 };
+      const mappedVarObj = Array.isArray(varobj) ? varobj.map(fn) : _.map(varobj, fn);
+      `
     }
   ]
 });
